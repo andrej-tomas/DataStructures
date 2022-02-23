ed9fed6c1d14962d6a6a396bb680c69ae5fd694f
Andrej Tomas <andrejtomas@aol.com>
2022-02-23 17:18:54 +0100

    Added a custom LinkedList class.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructures.LinkedList;
+
+MyLinkedList<int> list = new();
+list.Add(0);
+list.Add(1);
+list.Add(2);
+list.Add(3);
+
+Console.WriteLine("Last added item is: " + list.Peek());
+list.Print();
